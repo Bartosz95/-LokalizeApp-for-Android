8f27a6a568e352e853047fe901f826d288320d93
Bartosz Lenartowicz <bartek.lenart95@gmail.com>
2019-01-12 13:36:46 +0100

    control volume

--- app/src/main/java/com/example/home/projekt/Listening.java
+++ app/src/main/java/com/example/home/projekt/Listening.java
@@ -1,7 +1,9 @@
 package com.example.home.projekt;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.media.AudioManager;
 import android.media.VolumeProvider;
 import android.os.Binder;
 import android.os.IBinder;
@@ -20,6 +22,7 @@ public class Listening extends Service {
     int counter = 0;
     Toast toast;
 
+    private AudioManager audio;
     private MediaSessionCompat mediaSession;
 
     public class LocalBinder extends Binder {
@@ -37,34 +40,6 @@ public class Listening extends Service {
         mainActivity = callbacks;
     }
 
-    public class MyVolumeProvider extends VolumeProviderCompat{
-        public MyVolumeProvider(int volumeControl, int maxVolume, int currentVolume) {
-            super(volumeControl, maxVolume, currentVolume);
-        }
-        @Override
-        public void onAdjustVolume(int direction) {
-            super.onAdjustVolume(direction);
-            counter++;
-            if (counter == 1) {
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        try {
-                            Thread.sleep(5 * 1000);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        } finally {
-                            if ((counter > 80) && (mainActivity != null)){
-                                counter = 0;
-                                mainActivity.startAlarm();
-                            }
-                            counter = 0;
-                        }
-                    }
-                }).start();
-            }
-        }
-    };
     @Override
     public void onCreate() {
         super.onCreate();
@@ -72,16 +47,30 @@ public class Listening extends Service {
         counter=0;
         toast = Toast.makeText(this,"",Toast.LENGTH_SHORT);
 
+        audio = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+
         mediaSession = new MediaSessionCompat(this, "Listening");
         mediaSession.setFlags(MediaSessionCompat.FLAG_HANDLES_MEDIA_BUTTONS | MediaSessionCompat.FLAG_HANDLES_TRANSPORT_CONTROLS);
         mediaSession.setPlaybackState(new PlaybackStateCompat.Builder()
                 .setState(PlaybackStateCompat.STATE_PLAYING, 0, 0)
                 .build());
-        VolumeProviderCompat myVolumeProvider = new VolumeProviderCompat(VOLUME_CONTROL_RELATIVE ,100,50) {
+        VolumeProviderCompat myVolumeProvider = new VolumeProviderCompat(VOLUME_CONTROL_RELATIVE ,audio.getStreamMaxVolume(AudioManager.STREAM_MUSIC),audio.getStreamVolume(AudioManager.STREAM_MUSIC)) {
             @Override
             public void onAdjustVolume(int direction) {
                 super.onAdjustVolume(direction);
                 counter++;
+                this.setCurrentVolume(audio.getStreamVolume(AudioManager.STREAM_MUSIC));
+                    switch (direction) {
+                        case 1:
+                            audio.adjustStreamVolume(AudioManager.STREAM_MUSIC,
+                                    AudioManager.ADJUST_RAISE, AudioManager.FLAG_SHOW_UI);
+                            break;
+                        case -1:
+                            audio.adjustStreamVolume(AudioManager.STREAM_MUSIC,
+                                    AudioManager.ADJUST_LOWER, AudioManager.FLAG_SHOW_UI);
+                            break;
+                    }
+
                 if (counter == 1) {
                     new Thread(new Runnable() {
                         @Override
@@ -105,4 +94,5 @@ public class Listening extends Service {
         mediaSession.setPlaybackToRemote(myVolumeProvider);
         mediaSession.setActive(true);
     }
+
 }
